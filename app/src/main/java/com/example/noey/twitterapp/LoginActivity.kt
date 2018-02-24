94cc7838a935626f960033dfbcceaac8edc53209
noey <noey.@noey-4.local>
2018-02-24 23:19:42 +0700

    Added checking permission for access photo

--- app/src/main/java/com/example/noey/twitterapp/LoginActivity.kt
+++ app/src/main/java/com/example/noey/twitterapp/LoginActivity.kt
@@ -1,10 +1,10 @@
 package com.example.noey.twitterapp
 
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -14,6 +14,8 @@ import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(){
 
+    val REQUEST_CODE_PHOTO = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -22,13 +24,33 @@ class LoginActivity : AppCompatActivity(){
         btnLoginClick()
     }
 
-    fun checkPhotoPermission(){
+    fun checkImagePermission(){
         if(Build.VERSION.SDK_INT >= 23){
+            if(ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
 
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE_PHOTO)
+                return
+            }
         }
 
         loadImage()
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            REQUEST_CODE_PHOTO -> {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    loadImage()
+                } else{
+                    Toast.makeText(this, "Cannot access your images", Toast.LENGTH_LONG)
+                            .show()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
 
     private fun loadImage() {
 
@@ -36,6 +58,7 @@ class LoginActivity : AppCompatActivity(){
 
     fun profileImageClick(){
         imgProfile.setOnClickListener({
+            checkImagePermission()
             Toast.makeText(this, "image", Toast.LENGTH_LONG).show()
         })
     }
